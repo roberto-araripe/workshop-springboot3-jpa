86d06496f41cb18595f5876174ee73447afd3904
Roberto Araripe <roberto.araripe@outlook.com>
2022-10-28 10:28:47 -0300

    H2 database, test profile, JPA

--- src/main/java/com/tadkey/test/TestApplication.java
+++ src/main/java/com/tadkey/test/TestApplication.java
@@ -3,6 +3,7 @@ package com.tadkey.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class TestApplication {
 
