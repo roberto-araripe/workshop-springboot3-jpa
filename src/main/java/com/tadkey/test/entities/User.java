1a1fcc3639277039356c13ec7ebd1157787c89c7
Roberto Araripe <roberto.araripe@outlook.com>
2022-10-28 23:19:39 -0300

    Order, instant, ISO 8601 - PART 1

--- src/main/java/com/tadkey/test/entities/User.java
+++ src/main/java/com/tadkey/test/entities/User.java
@@ -1,11 +1,16 @@
 package com.tadkey.test.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 
@@ -22,6 +27,10 @@ public class User implements Serializable {
 	private String phone;
 	private String password;
 	
+	@JsonIgnore
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
 	public User() {
 	}
 
@@ -33,7 +42,7 @@ public class User implements Serializable {
 		this.phone = phone;
 		this.password = password;
 	}
-
+	
 	public Long getId() {
 		return id;
 	}
@@ -74,6 +83,11 @@ public class User implements Serializable {
 		this.password = password;
 	}
 
+
+	public List<Order> getOrders() {
+		return orders;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -98,5 +112,4 @@ public class User implements Serializable {
 			return false;
 		return true;
 	}
-	 
 }
