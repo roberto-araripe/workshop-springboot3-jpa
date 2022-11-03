af37db00f2f9c36a3b1548d088fb79cbfbd2680b
Roberto Araripe <roberto.araripe@outlook.com>
2022-11-03 16:00:06 -0300

    Exception handling - delete

--- src/main/java/com/tadkey/test/services/UserService.java
+++ src/main/java/com/tadkey/test/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.tadkey.test.entities.User;
 import com.tadkey.test.repositories.UserRepository;
+import com.tadkey.test.resources.exceptions.DatabaseException;
 import com.tadkey.test.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -29,8 +32,13 @@ public class UserService {
 		return repository.save(obj);
 	}
 	public void delete(Long id) {
-		repository.deleteById(id);
-		
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	public User update (Long id, User obj) {
 		User entity = repository.getReferenceById(id);
